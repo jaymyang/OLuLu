81e1015383abc0eb316c726ad47b9877bd31fe3e
Jay Ming-chieh Yang <68020453+jaymyang@users.noreply.github.com>
2025-02-21 17:35:47 +0800

    Update HX711.h

--- HX711.h
+++ HX711.h
@@ -3,8 +3,8 @@
 
 #include <Arduino.h>
 
-#define HX711_SCK 2
-#define HX711_DT 15 //NodeMCU-32S的D3是無法做PWM的，改選D15或其他D-pin
+#define HX711_SCK 2 //MCU32 用2，esp8266用13
+#define HX711_DT 15 //NodeMCU-32S的D3是無法做PWM的，改選D15或其他D-pin；ESP8266用12
 
 extern void Init_Hx711();
 extern unsigned long HX711_Read(void);
